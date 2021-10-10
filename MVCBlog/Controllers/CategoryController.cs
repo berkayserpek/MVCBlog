4ce2787157954971dc234aeceab050b61567f23c
Mehmet Berkay Serpek <berkayserpek@gmail.com>
2021-10-11 01:06:32 +0300

    Add methods in repository class.

---
+++ MVCBlog/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        public ActionResult Index()
+        {
+            var categoryValues = categoryRepository.GetAll();
+            return View(categoryValues);
+        }
+    }
+}
