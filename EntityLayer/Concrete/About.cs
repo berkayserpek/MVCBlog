54bd4feaaf81731c6804c0f56456367d58d5f1d4
Mehmet Berkay Serpek <berkayserpek@gmail.com>
2021-10-10 04:50:29 +0300

    Add project files.

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutID { get; set; }
+        [StringLength(500)]
+        public string AboutContentFirst { get; set; }
+        [StringLength(500)]
+        public string AboutContetSecond { get; set; }
+        [StringLength(100)]
+        public string AboutImageFirst { get; set; }
+        [StringLength(100)]
+        public string AboutImageSecond { get; set; }
+
+    }
+}
